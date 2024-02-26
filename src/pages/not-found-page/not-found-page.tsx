048a364a368708cf4682646a2b748b1a9fd6ef75
Sofya <SofjaEsenina@yandex.ru>
2024-02-26 07:19:25 +0300

    3.1 Страница Page Not Found

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,25 @@
+function NotFound() : JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">404 PAGE NOT FOUND</h1>
+            <a>
+              <h2>Вернуться на главную</h2>
+            </a>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFound;
