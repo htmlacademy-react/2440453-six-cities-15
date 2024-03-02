b3118fb99251b69d616370a7204b00d1095ea364
Sofya <SofjaEsenina@yandex.ru>
2024-03-02 10:21:41 +0300

    4.2 Типы для пользователя, отзыва и моки

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,47 @@
+
+
+import { TReviewList } from '../types';
+
+const REVIEW_LIST: TReviewList = [
+  {
+    'id': 1,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://14.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 3,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': new Date('2023-11-02T11:13:12.982Z'),
+    'offerId': '1',
+  },
+  {
+    'id': 3,
+    'user': {
+      'id': 19,
+      'isPro': false,
+      'name': 'Christina',
+      'avatarUrl': 'https://14.react.pages.academy/static/avatar/10.jpg'
+    },
+    'rating': 4,
+    'comment': 'This villa is perfect in every way: the view on mountains and waterfalls, the hot tub and the villa itself. The evening here became a great continuation of our journeys over country.',
+    'date': new Date('2023-10-10T11:13:12.982Z'),
+    'offerId': '3',
+  },
+  {
+    'id': 2,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://14.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 3,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': new Date('2023-10-10T11:13:12.982Z'),
+    offerId: '3',
+  },
+];
+
+export default REVIEW_LIST;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import { THost, TOffer, TOfferList, TOfferFull } from './offer';
 import { TCity, TLocation } from './city';
+import { TUser, TAuthorisation, TUserData, TUserAuthorisation } from './user';
+import { TCommentData, TOfferReview, TReview, TReviewList } from './review';
 
 export type {
   THost,
@@ -8,4 +10,12 @@ export type {
   TOffer,
   TOfferFull,
   TOfferList,
+  TUser,
+  TAuthorisation,
+  TUserData,
+  TUserAuthorisation,
+  TCommentData,
+  TOfferReview,
+  TReview,
+  TReviewList,
 };
