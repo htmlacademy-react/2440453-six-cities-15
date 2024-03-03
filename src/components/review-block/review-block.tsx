1032eebd90d9563e4f64d4902a0c845839ca87f9
Sofya <SofjaEsenina@yandex.ru>
2024-03-03 15:22:57 +0300

    4.9 Форма ввода - только для авторизованных

--- src/components/review-block/review-block.tsx
+++ src/components/review-block/review-block.tsx
@@ -4,10 +4,11 @@ import ReviewList from '../review-list/review-list';
 
 function ReviewBlock() : JSX.Element {
   const reviewList = REVIEWS_LIST;
+  const isAuth = true;
   return(
     <section className="offer__reviews reviews">
       <ReviewList reviewList={reviewList}/>
-      <CommentForm/>
+      {isAuth && <CommentForm/>}
     </section>
   );
 }
