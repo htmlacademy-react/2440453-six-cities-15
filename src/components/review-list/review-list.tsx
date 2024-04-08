a9dcf85751cf67c38602cea261e61c0433b3c065
Sofya <SofjaEsenina@yandex.ru>
2024-04-08 18:36:58 +0300

    7.3.6 Поправила ошибку с сортировкой

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -1,6 +1,6 @@
 import { MAX_REVIEW_COUNT } from '../../consts';
 import { TReviewList } from '../../types';
-import { getCount } from '../../utils';
+import { getCount, setCorrectEnding } from '../../utils';
 import ReviewItem from './review-item';
 
 type TReviewListProps = {
@@ -9,7 +9,8 @@ type TReviewListProps = {
 
 function ReviewList({reviewList}: TReviewListProps) : JSX.Element {
   const reviewCount = getCount(reviewList.length, MAX_REVIEW_COUNT);
-  const sortedReviews = reviewList.sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf());
+  const sortedReviews = reviewList && reviewList.length > 0 ? [...reviewList] : [];
+  sortedReviews.sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf());
   const reviews = sortedReviews.map((item, i) => {
     if (i < reviewCount) {
       return <ReviewItem key={item.id} item={item}/>;
@@ -17,7 +18,7 @@ function ReviewList({reviewList}: TReviewListProps) : JSX.Element {
   });
   return(
     <>
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviewList.length}</span></h2>
+      <h2 className="reviews__title">{`Review${setCorrectEnding(reviewList.length)}`} &middot; <span className="reviews__amount">{reviewList.length}</span></h2>
       <ul className="reviews__list">
         {reviews}
       </ul>
